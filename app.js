c33e2dedd9871bbb635e035abd81f118bb0a9662
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-10-05 08:51:49 +0300

    project init

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let barcode = '';
+let scanningInterval = null;
+
+const handleBarcode = () => {
+  const div = document.querySelector('#code');
+
+  div.innerHTML = barcode
+};
+
+document.addEventListener('keydown', (event) => {
+  if (scanningInterval) {
+    clearInterval(scanningInterval);
+  }
+
+  if (event.code === 'Enter') {
+    if (barcode) {
+      handleBarcode();
+
+      barcode = '';
+      return;
+    }
+
+    if (event.key !== 'Shift') {
+      barcode += event.key;
+    }
+
+    interval = setInterval(() => {
+      barcode = '';
+    }, 100)
+  }
+});
